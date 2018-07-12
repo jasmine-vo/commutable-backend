77deb2a790e71fa6d54af300f50467d81ec1010d
Jasmine Vo <jasmine@Jasmines-MacBook-Pro.local>
2018-07-11 19:06:04 -0700

    add indeed publisher id

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const config = {
   bartApiKey: process.env.BART_API_KEY,
+  indeedPublisherID: process.env.INDEED_PUBLISHER_ID,
   PORT: process.env.PORT || 3001
 };
 
